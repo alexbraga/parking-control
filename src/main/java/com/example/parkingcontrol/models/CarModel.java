f9cf586b47aa9d6e7dca0b1806d24044f9d5ecca
Alex Braga <contato@alexbraga.com.br>
2022-09-07 03:36:54 -0300

    Update ParkingSpotModel and create CarModel

---
+++ src/main/java/com/example/parkingcontrol/models/CarModel.java
@@ -0,0 +1,73 @@
+package com.example.parkingcontrol.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "car")
+public class CarModel implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlate;
+    @Column(nullable = false, length = 70)
+    private String carBrand;
+    @Column(nullable = false, length = 70)
+    private String carModel;
+    @Column(nullable = false, length = 70)
+    private String carColor;
+    @OneToOne(mappedBy = "car")
+    private ParkingSpotModel parkingSpot;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public ParkingSpotModel getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(ParkingSpotModel parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+}
--- src/main/java/com/example/parkingcontrol/models/ParkingSpotModel.java
+++ src/main/java/com/example/parkingcontrol/models/ParkingSpotModel.java
@@ -1,7 +1,85 @@
 package com.example.parkingcontrol.models;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
-public class ParkingSpotModel {
+@Table(name = "parking_spot")
+public class ParkingSpotModel implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotNumber;
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+    @Column(nullable = false, length = 130)
+    private String owner;
+    @Column(nullable = false, length = 30)
+    private String apartment;
+    @Column(nullable = false, length = 30)
+    private String block;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "car_id")
+    private CarModel car;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getParkingSpotNumber() {
+        return parkingSpotNumber;
+    }
+
+    public void setParkingSpotNumber(String parkingSpotNumber) {
+        this.parkingSpotNumber = parkingSpotNumber;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
+
+    public CarModel getCar() {
+        return car;
+    }
+
+    public void setCar(CarModel car) {
+        this.car = car;
+    }
 }
