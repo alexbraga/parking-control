9b913fa9a3e88a3c5a53c02f795abaa558ceade2
Alex Braga <contato@alexbraga.com.br>
2022-09-08 01:51:17 -0300

    Generate getters and setters for ParkingSpotDTO and CarDTO

--- src/main/java/com/example/parkingcontrol/dtos/CarDTO.java
+++ src/main/java/com/example/parkingcontrol/dtos/CarDTO.java
@@ -13,4 +13,36 @@ public class CarDTO {
     private String carModel;
     @NotBlank
     private String carColor;
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
 }
--- src/main/java/com/example/parkingcontrol/dtos/ParkingSpotDTO.java
+++ src/main/java/com/example/parkingcontrol/dtos/ParkingSpotDTO.java
@@ -11,4 +11,36 @@ public class ParkingSpotDTO {
     private String apartment;
     @NotBlank
     private String block;
+
+    public String getParkingSpotNumber() {
+        return parkingSpotNumber;
+    }
+
+    public void setParkingSpotNumber(String parkingSpotNumber) {
+        this.parkingSpotNumber = parkingSpotNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
 }
