aca98aa3f541b987abc90eb166d75a2b62855897
Alex Braga <contato@alexbraga.com.br>
2022-09-08 01:43:23 -0300

    Create CarController and rename RequestMapping path on ParkingSpotController

--- src/main/java/com/example/parkingcontrol/controllers/CarController.java
+++ src/main/java/com/example/parkingcontrol/controllers/CarController.java
@@ -1,15 +1,10 @@
 package com.example.parkingcontrol.controllers;
 
-import com.example.parkingcontrol.services.ParkingSpotService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/parking-lot")
-public class ParkingSpotController {
-    @Autowired
-    ParkingSpotService parkingSpotService;
+public class CarController {
 }
