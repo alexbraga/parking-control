dd4d5409a903c85fd052cc31470787eee6508954
Alex Braga <contato@alexbraga.com.br>
2022-09-16 01:42:45 -0300

    Update ParkingSpotRepository with findByParkingSpotNumber, findByApartment and findByOwner methods

--- src/main/java/com/example/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/example/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -3,8 +3,13 @@ package com.example.parkingcontrol.repositories;
 import com.example.parkingcontrol.models.ParkingSpotModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+    Optional<ParkingSpotModel> findByParkingSpotNumber(String parkingSpotNumber);
 
+    Optional<ParkingSpotModel> findByApartment(String apartment);
+
+    Optional<ParkingSpotModel> findByOwner(String owner);
 }
