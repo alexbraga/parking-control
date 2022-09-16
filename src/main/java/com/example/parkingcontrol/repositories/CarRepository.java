9dbad1fc3afbb4fe224192ad567a3561355ef75a
Alex Braga <contato@alexbraga.com.br>
2022-09-16 03:38:27 -0300

    Update CarRepository with findByLicensePlate method

--- src/main/java/com/example/parkingcontrol/repositories/CarRepository.java
+++ src/main/java/com/example/parkingcontrol/repositories/CarRepository.java
@@ -3,8 +3,10 @@ package com.example.parkingcontrol.repositories;
 import com.example.parkingcontrol.models.CarModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CarRepository extends JpaRepository<CarModel, UUID> {
 
+    Optional<CarModel> findByLicensePlate(String licensePlate);
 }
