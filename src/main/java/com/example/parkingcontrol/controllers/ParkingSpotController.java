15d8945f366c5140e806ba14778bc7a818b73a01
Alex Braga <contato@alexbraga.com.br>
2022-10-04 04:32:47 -0300

    Create ParkingSpotControllerTest, write tests and implement ParkingSpotController methods

--- src/main/java/com/example/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/example/parkingcontrol/controllers/ParkingSpotController.java
@@ -1,15 +1,117 @@
 package com.example.parkingcontrol.controllers;
 
+import com.example.parkingcontrol.dtos.ParkingSpotDTO;
+import com.example.parkingcontrol.models.ParkingSpotModel;
 import com.example.parkingcontrol.services.ParkingSpotService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/parking-lot")
+@RequestMapping("/parking-spot")
 public class ParkingSpotController {
     @Autowired
     ParkingSpotService parkingSpotService;
+
+    @PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDTO parkingSpotDTO) {
+        if (parkingSpotService.existsBySpotNumber(parkingSpotDTO.getSpotNumber())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot is already in use!");
+        }
+
+        if (parkingSpotService.existsByApartmentAndBlock(parkingSpotDTO.getApartment(), parkingSpotDTO.getBlock())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Parking Spot is already registered for this apartment and block!");
+        }
+
+        ParkingSpotModel parkingSpot = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDTO, parkingSpot);
+        parkingSpot.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpot));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ParkingSpotModel>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> findById(@PathVariable(value = "id") UUID id) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found.");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+    }
+
+    @GetMapping("/spot-number")
+    public ResponseEntity<Object> findBySpotNumber(@RequestParam(defaultValue = "") String spot) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findByParkingSpotNumber(spot);
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found.");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+    }
+
+    @GetMapping("/apartment")
+    public ResponseEntity<Object> findByApartment(@RequestParam(defaultValue = "") String number) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findByApartment(number);
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found.");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+    }
+
+    @GetMapping("/owner")
+    public ResponseEntity<Object> findByOwner(@RequestParam(defaultValue = "") String name) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findByOwner(name);
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found.");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> update(@PathVariable(value = "id") UUID id, @RequestBody @Valid ParkingSpotDTO parkingSpotDTO) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found.");
+        }
+
+        ParkingSpotModel parkingSpot = parkingSpotModelOptional.get();
+        parkingSpot.setSpotNumber(parkingSpotDTO.getSpotNumber());
+        parkingSpot.setApartment(parkingSpotDTO.getApartment());
+        parkingSpot.setBlock(parkingSpotDTO.getBlock());
+        parkingSpot.setOwner(parkingSpotDTO.getOwner());
+
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpot));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Object> delete(@PathVariable(value = "id") UUID id) {
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found.");
+        }
+
+        parkingSpotService.delete(parkingSpotModelOptional.get().getId());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Parking spot deleted successfully.");
+    }
 }
---
+++ src/test/java/com/example/parkingcontrol/controllers/ParkingSpotControllerTest.java
@@ -0,0 +1,297 @@
+package com.example.parkingcontrol.controllers;
+
+import com.example.parkingcontrol.dtos.ParkingSpotDTO;
+import com.example.parkingcontrol.models.ParkingSpotModel;
+import com.example.parkingcontrol.services.ParkingSpotService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ParkingSpotController.class)
+@ActiveProfiles("test")
+class ParkingSpotControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    ParkingSpotService parkingSpotService;
+
+    @Test
+    void shouldSaveNewParkingSpot() throws Exception {
+        // Given
+        ParkingSpotDTO parkingSpotDTO = new ParkingSpotDTO();
+        parkingSpotDTO.setSpotNumber("701-A");
+        parkingSpotDTO.setApartment("701");
+        parkingSpotDTO.setBlock("I");
+        parkingSpotDTO.setOwner("Jade");
+
+        ParkingSpotModel parkingSpot = new ParkingSpotModel();
+        parkingSpot.setId(UUID.fromString("0a96e04e-b60f-4b69-9524-e221cf341ccb"));
+
+        BeanUtils.copyProperties(parkingSpotDTO, parkingSpot);
+
+        // When
+        Mockito.when(parkingSpotService.save(any(ParkingSpotModel.class))).thenReturn(parkingSpot);
+
+        // Then
+        mockMvc.perform(post("/parking-spot")
+                                .contentType("application/json")
+                                .content(objectMapper.writeValueAsString(parkingSpotDTO)))
+               .andExpect(status().isCreated())
+               .andExpect(jsonPath("$.id", is("0a96e04e-b60f-4b69-9524-e221cf341ccb")));
+    }
+
+    @Test
+    void shouldFailSavingWhenSpotNumberAlreadyInUse() throws Exception {
+        // Given
+        ParkingSpotDTO parkingSpotDTO = new ParkingSpotDTO();
+        parkingSpotDTO.setSpotNumber("701-A");
+        parkingSpotDTO.setApartment("701");
+        parkingSpotDTO.setBlock("I");
+        parkingSpotDTO.setOwner("Jade");
+
+        // When
+        Mockito.when(parkingSpotService.existsBySpotNumber(parkingSpotDTO.getSpotNumber())).thenReturn(true);
+
+        // Then
+        mockMvc.perform(post("/parking-spot")
+                                .contentType("application/json")
+                                .content(objectMapper.writeValueAsString(parkingSpotDTO)))
+               .andExpect(status().isConflict())
+               .andExpect(content().string("Conflict: Parking Spot is already in use!"));
+    }
+
+    @Test
+    void shouldFailSavingWhenApartmentAndBlockAlreadyInUse() throws Exception {
+        // Given
+        ParkingSpotDTO parkingSpotDTO = new ParkingSpotDTO();
+        parkingSpotDTO.setSpotNumber("701-A");
+        parkingSpotDTO.setApartment("701");
+        parkingSpotDTO.setBlock("I");
+        parkingSpotDTO.setOwner("Jade");
+
+        // When
+        Mockito.when(parkingSpotService.existsByApartmentAndBlock(parkingSpotDTO.getApartment(), parkingSpotDTO.getBlock()))
+               .thenReturn(true);
+
+        // Then
+        mockMvc.perform(post("/parking-spot")
+                                .contentType("application/json")
+                                .content(objectMapper.writeValueAsString(parkingSpotDTO)))
+               .andExpect(status().isConflict())
+               .andExpect(content().string("Conflict: Parking Spot is already registered for this apartment and block!"));
+    }
+
+    @Test
+    void shouldGetAllParkingSpots() throws Exception {
+        mockMvc.perform(get("/parking-spot")).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldFindParkingSpotById() throws Exception {
+        // Given
+        ParkingSpotModel parkingSpot = new ParkingSpotModel();
+        parkingSpot.setId(UUID.fromString("0a96e04e-b60f-4b69-9524-e221cf341ccb"));
+
+        // When
+        Mockito.when(parkingSpotService.findById(UUID.fromString("0a96e04e-b60f-4b69-9524-e221cf341ccb"))).thenReturn(
+                Optional.of(parkingSpot));
+
+        // Then
+        mockMvc.perform(get("/parking-spot/0a96e04e-b60f-4b69-9524-e221cf341ccb")).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldFailWhenParkingSpotNotFoundById() throws Exception {
+        // When
+        Mockito.when(parkingSpotService.findById(UUID.fromString("0a96e04e-b60f-4b69-9524-e221cf341ccb"))).thenReturn(
+                Optional.empty());
+
+        // Then
+        mockMvc.perform(get("/parking-spot/0a96e04e-b60f-4b69-9524-e221cf341ccb"))
+               .andExpect(status().isNotFound())
+               .andExpect(content().string("Parking spot not found."));
+    }
+
+    @Test
+    void shouldFindParkingSpotBySpotNumber() throws Exception {
+        // Given
+        ParkingSpotModel parkingSpot = new ParkingSpotModel();
+        parkingSpot.setSpotNumber("701-A");
+
+        // When
+        Mockito.when(parkingSpotService.findByParkingSpotNumber("701-A")).thenReturn(Optional.of(parkingSpot));
+
+        // Then
+        mockMvc.perform(get("/parking-spot/spot-number?spot=701-A")).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldFailWhenParkingSpotNotFoundBySpotNumber() throws Exception {
+        // When
+        Mockito.when(parkingSpotService.findByParkingSpotNumber("701")).thenReturn(Optional.empty());
+
+        // Then
+        mockMvc.perform(get("/parking-spot/spot-number?spot=701"))
+               .andExpect(status().isNotFound())
+               .andExpect(content().string("Parking spot not found."));
+    }
+
+    @Test
+    void shouldFindParkingSpotByApartment() throws Exception {
+        // Given
+        ParkingSpotModel parkingSpot = new ParkingSpotModel();
+        parkingSpot.setApartment("701");
+
+        // When
+        Mockito.when(parkingSpotService.findByApartment("701")).thenReturn(Optional.of(parkingSpot));
+
+        // Then
+        mockMvc.perform(get("/parking-spot/apartment?number=701")).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldFailWhenParkingSpotNotFoundByApartment() throws Exception {
+        // When
+        Mockito.when(parkingSpotService.findByApartment("603")).thenReturn(Optional.empty());
+
+        // Then
+        mockMvc.perform(get("/parking-spot/apartment?number=603"))
+               .andExpect(status().isNotFound())
+               .andExpect(content().string("Parking spot not found."));
+    }
+
+    @Test
+    void shouldFindParkingSpotByOwner() throws Exception {
+        // Given
+        ParkingSpotModel parkingSpot = new ParkingSpotModel();
+        parkingSpot.setOwner("Jade");
+
+        // When
+        Mockito.when(parkingSpotService.findByOwner("Jade")).thenReturn(Optional.of(parkingSpot));
+
+        // Then
+        mockMvc.perform(get("/parking-spot/owner?name=Jade")).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldFailWhenParkingSpotNotFoundByOwner() throws Exception {
+        // When
+        Mockito.when(parkingSpotService.findByOwner("Jessica")).thenReturn(Optional.empty());
+
+        // Then
+        mockMvc.perform(get("/parking-spot/owner?name=Jessica"))
+               .andExpect(status().isNotFound())
+               .andExpect(content().string("Parking spot not found."));
+    }
+
+    @Test
+    void shouldUpdateParkingSpot() throws Exception {
+        // Given
+        ParkingSpotDTO parkingSpotDTO = new ParkingSpotDTO();
+        parkingSpotDTO.setSpotNumber("701-A");
+        parkingSpotDTO.setApartment("701");
+        parkingSpotDTO.setBlock("I");
+        parkingSpotDTO.setOwner("Jade");
+
+        ParkingSpotModel parkingSpot = new ParkingSpotModel();
+        parkingSpot.setId(UUID.fromString("0a96e04e-b60f-4b69-9524-e221cf341ccb"));
+
+        // When
+        Mockito.when(parkingSpotService.findById(UUID.fromString("0a96e04e-b60f-4b69-9524-e221cf341ccb")))
+               .thenReturn(Optional.of(parkingSpot));
+
+        Mockito.when(parkingSpotService.save(parkingSpot)).thenReturn(parkingSpot);
+
+        BeanUtils.copyProperties(parkingSpotDTO, parkingSpot);
+
+        // Then
+        mockMvc.perform(put("/parking-spot/update/0a96e04e-b60f-4b69-9524-e221cf341ccb")
+                                .contentType("application/json")
+                                .content(objectMapper.writeValueAsString(parkingSpotDTO)))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.id", is("0a96e04e-b60f-4b69-9524-e221cf341ccb")))
+               .andExpect(content().string(objectMapper.writeValueAsString(parkingSpot)));
+    }
+
+    @Test
+    void shouldFailUpdateWhenInvalidParkingSpotDTOSubmitted() throws Exception {
+        // Given
+        ParkingSpotDTO parkingSpotDTO = new ParkingSpotDTO();
+        parkingSpotDTO.setSpotNumber("");
+        parkingSpotDTO.setApartment("701");
+        parkingSpotDTO.setBlock("I");
+        parkingSpotDTO.setOwner("Jade");
+
+        // Then
+        mockMvc.perform(put("/parking-spot/update/0a96e04e-b60f-4b69-9524-e221cf341ccb")
+                                .contentType("application/json")
+                                .content(objectMapper.writeValueAsString(parkingSpotDTO)))
+               .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldFailUpdateWhenParkingSpotNotFound() throws Exception {
+        // Given
+        ParkingSpotDTO parkingSpotDTO = new ParkingSpotDTO();
+        parkingSpotDTO.setSpotNumber("701-A");
+        parkingSpotDTO.setApartment("701");
+        parkingSpotDTO.setBlock("I");
+        parkingSpotDTO.setOwner("Jade");
+
+        // Then
+        mockMvc.perform(put("/parking-spot/update/0a96e04e-b60f-4b69-9524-e221cf341ccb")
+                                .contentType("application/json")
+                                .content(objectMapper.writeValueAsString(parkingSpotDTO)))
+               .andExpect(status().isNotFound())
+               .andExpect(content().string("Parking spot not found."));
+    }
+
+    @Test
+    void shouldDeleteParkingSpot() throws Exception {
+        // Given
+        UUID id = UUID.fromString("0a96e04e-b60f-4b69-9524-e221cf341ccb");
+        ParkingSpotModel parkingSpot = new ParkingSpotModel();
+        parkingSpot.setId(id);
+
+        ParkingSpotService serviceSpy = Mockito.spy(parkingSpotService);
+
+        // When
+        Mockito.when(parkingSpotService.findById(id)).thenReturn(Optional.of(parkingSpot));
+        Mockito.doNothing().when(serviceSpy).delete(id);
+
+        // Then
+        mockMvc.perform(delete("/parking-spot/delete/0a96e04e-b60f-4b69-9524-e221cf341ccb")
+                                .contentType("application/json"))
+               .andExpect(status().isNoContent());
+
+        verify(parkingSpotService, times(1)).delete(id);
+    }
+
+    @Test
+    void shouldFailDeleteWhenParkingSpotNotFound() throws Exception {
+        mockMvc.perform(delete("/parking-spot/delete/0a96e04e-b60f-4b69-9524-e221cf341ccb")
+                                .contentType("application/json"))
+               .andExpect(status().isNotFound())
+               .andExpect(content().string("Parking spot not found."));
+    }
+}
