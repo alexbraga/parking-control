3fa013a7d4c15e06a1ca9aaeb7db1d65ab9af8f7
Alex Braga <contato@alexbraga.com.br>
2022-10-05 20:33:22 -0300

    Implement save method for POST request

--- src/main/java/com/example/parkingcontrol/controllers/CarController.java
+++ src/main/java/com/example/parkingcontrol/controllers/CarController.java
@@ -1,10 +1,28 @@
 package com.example.parkingcontrol.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.parkingcontrol.dtos.CarDTO;
+import com.example.parkingcontrol.models.CarModel;
+import com.example.parkingcontrol.services.CarService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/cars")
 public class CarController {
+    @Autowired
+    private CarService carService;
+
+    @PostMapping
+    public ResponseEntity<Object> saveCar(@RequestBody @Valid CarDTO carDTO) {
+        CarModel carModel = new CarModel();
+        BeanUtils.copyProperties(carDTO, carModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(carService.save(carModel));
+    }
 }
